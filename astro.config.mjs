c778679a617157a42c2894af867244d57f70eb21
Felipe Torres <felipe.torressepulveda@gmail.com>
2023-10-12 20:30:37 -0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    sanity({
+      projectId: "dsb0by8m",
+      dataset: "production",
+      apiVersion: "2023-02-08",
+      useCdn: false,
+      studioBasePath: "/admin",
+    }),
+    react(),
+  ],
 });
