45c6afb427d1059a9f7fde6c0d1ff3c4c1cb5036
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2023-10-24 20:35:17 -0700

    Landing — Logo Idea — Fancy Animation 🎩 (#6)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,6 +16,13 @@ export default {
         "custom-gradient":
           "linear-gradient(140deg, rgba(10,10,10,1) 0%, rgba(49,27,107,1) 100%)",
       },
+      dropShadow: {
+        "fancy-text": ["0 5px 10px rgba(255, 255, 255, 0.10)"],
+        "fancy-icon-shadow": [
+          "-5px 0px 15px rgba(255, 255, 255, 0.10)",
+          "5px 0px 15px rgba(255, 255, 255, 0.10)",
+        ],
+      },
     },
   },
   plugins: [],
